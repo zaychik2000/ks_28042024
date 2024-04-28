557eddd000554654a9b42fe388967813db5ee88b
zaychik2000 <liliyacutie50@gmail.com>
2024-04-28 18:43:27 +0300

    ks-1-1 changed the name of the variables and indicated their type

--- src/main/kotlin/lesson1/lesson1_task1.kt
+++ src/main/kotlin/lesson1/lesson1_task1.kt
@@ -1,7 +1,7 @@
 package org.example.lesson1
 
 fun main(){
-    val theNumberOfOrders = 75
-    val Gratitude = "Благодарим вас за этот выбор! :) Вы можете принять участие в акции."
-    println("Вы осуществили $theNumberOfOrders заказов в нашем магазине! \n$Gratitude")
+    val theNumberOfOrders: Int = 75
+    val ThanksForThePurchase: String = "Благодарим вас за этот выбор! :) Вы можете принять участие в акции."
+    println("Вы осуществили $theNumberOfOrders заказов в нашем магазине! \n$ThanksForThePurchase")
 }
