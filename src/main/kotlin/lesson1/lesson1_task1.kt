1649d4033b2755feecbb7e4902bf3be2440cf1cf
zaychik2000 <liliyacutie50@gmail.com>
2024-04-28 18:29:22 +0300

    ks-1-1 wrote the application screen logic for an online store

---
+++ src/main/kotlin/lesson1/lesson1_task1.kt
@@ -0,0 +1,7 @@
+package org.example.lesson1
+
+fun main(){
+    val theNumberOfOrders = 75
+    val Gratitude = "Благодарим вас за этот выбор! :) Вы можете принять участие в акции."
+    println("Вы осуществили $theNumberOfOrders заказов в нашем магазине! \n$Gratitude")
+}
