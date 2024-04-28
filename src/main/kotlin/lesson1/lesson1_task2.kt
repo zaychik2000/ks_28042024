0552ccc3d7961e478790d2652367db7057b26230
zaychik2000 <liliyacutie50@gmail.com>
2024-04-28 19:02:51 +0300

    ks-1-2 I simulate logs from the server

---
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson1
+
+fun main(){
+
+    val theNumberOfOrders: Int = 75
+
+    val ThanksForThePurchase: String = "Благодарим вас за этот выбор! :) Вы можете принять участие в акции."
+    println("Вы осуществили $theNumberOfOrders заказов в нашем магазине! \n$ThanksForThePurchase")
+
+    var amountOfWorkers = 2000
+    //println(amountOfWorkers)
+
+    amountOfWorkers = 1999
+    println(amountOfWorkers)
+}
