dd72600b90e6054d14e2f0eb8a2c89bc6757e0db
zaychik2000 <liliyacutie50@gmail.com>
2024-04-28 19:00:13 +0300

    ks-1-1 added spaces

--- src/main/kotlin/lesson1/lesson1_task1.kt
+++ src/main/kotlin/lesson1/lesson1_task1.kt
@@ -1,7 +1,10 @@
 package org.example.lesson1
 
 fun main(){
+
     val theNumberOfOrders: Int = 75
+
     val ThanksForThePurchase: String = "Благодарим вас за этот выбор! :) Вы можете принять участие в акции."
+
     println("Вы осуществили $theNumberOfOrders заказов в нашем магазине! \n$ThanksForThePurchase")
 }
