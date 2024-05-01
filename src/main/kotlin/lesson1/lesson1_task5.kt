050f45e7a743b5261c6867e6dfb125a89b1ac1db
zaychik2000 <liliyacutie50@gmail.com>
2024-05-01 17:21:35 +0300

    ks-1-5 removed magic numbers and improved design

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -2,10 +2,13 @@ package org.example.lesson1
 
 fun main(){
 
+    val secondsInAnHour = 3600
+    val secondInAnMinute = 60
     val totalseconds = 6480
-    val hours = totalseconds / 3600
-    val minuts = (totalseconds % 3600)/60
-    val seconds = totalseconds % 60
+
+    val hours = totalseconds / secondsInAnHour
+    val minuts = (totalseconds % secondsInAnHour) / secondInAnMinute
+    val seconds = totalseconds % secondInAnMinute
 
     val formattedTime = "%02d:%02d:%02d".format(hours, minuts, seconds)
     println("Время, проведенное в космосе: \n$formattedTime")
