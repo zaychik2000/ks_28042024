cb6bfe06e16e24b8a8a8164420e49c7d12348da4
zaychik2000 <liliyacutie50@gmail.com>
2024-05-01 17:00:14 +0300

    ks-1-2 capitalized variable

--- src/main/kotlin/lesson1/lesson1_task2.kt
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -4,8 +4,8 @@ fun main(){
 
     val theNumberOfOrders: Int = 75
 
-    val ThanksForThePurchase: String = "Благодарим вас за этот выбор! :) Вы можете принять участие в акции."
-    println("Вы осуществили $theNumberOfOrders заказов в нашем магазине! \n$ThanksForThePurchase")
+    val thanksForThePurchase: String = "Благодарим вас за этот выбор! :) Вы можете принять участие в акции."
+    println("Вы осуществили $theNumberOfOrders заказов в нашем магазине! \n$thanksForThePurchase")
 
     var amountOfWorkers: Int = 2000
     //println(amountOfWorkers)
