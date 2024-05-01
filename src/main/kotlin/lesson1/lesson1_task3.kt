46841c70ae2cfa3a69a871919fa7a70c1f13c427
zaychik2000 <liliyacutie50@gmail.com>
2024-05-01 15:02:39 +0300

    ks-1-3 changed the format and type

--- src/main/kotlin/lesson1/lesson1_task3.kt
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -2,15 +2,15 @@ package org.example.lesson1
 
 fun main(){
 
-    val year = 1961
+    val year = "1961"
 
-    var minute = 7
-    var hour = 9
+    var minute = "07"
+    var hour = "09"
 
     println("Взлет: \n$year год \n$hour часов \n$minute минут")
 
-    minute = 55
-    hour = 10
+    minute = "55"
+    hour = "10"
 
     println("\nПосадка: \n${year}г $hour:$minute")
 }
