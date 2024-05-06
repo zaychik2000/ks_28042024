6e5a9d547e40645a7209622c61dd97812d5b3616
zaychik2000 <liliyacutie50@gmail.com>
2024-05-06 06:18:58 +0300

    ks-1-5 changed the variables to constants

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,15 +1,15 @@
 package org.example.lesson1
 
-fun main(){
+fun main() {
 
-    val secondsInAnHour = 3600
-    val secondInAnMinute = 60
-    val totalseconds = 6480
-
-    val hours = totalseconds / secondsInAnHour
-    val minuts = (totalseconds % secondsInAnHour) / secondInAnMinute
-    val seconds = totalseconds % secondInAnMinute
+    val hours = TOTAL_SECONDS / SECOND_IN_AN_HOUR
+    val minuts = (TOTAL_SECONDS % SECOND_IN_AN_HOUR) / SECOND_IN_AN_MINUTE
+    val seconds = TOTAL_SECONDS % SECOND_IN_AN_MINUTE
 
     val formattedTime = "%02d:%02d:%02d".format(hours, minuts, seconds)
     println("Время, проведенное в космосе: \n$formattedTime")
 }
+
+val SECOND_IN_AN_HOUR = 3600
+val SECOND_IN_AN_MINUTE = 60
+val TOTAL_SECONDS = 6480
