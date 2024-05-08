90f0f16c913f51eddc69c83b0e24587fbe6b9aba
zaychik2000 <liliyacutie50@gmail.com>
2024-05-08 07:46:35 +0300

    ks-2-3

---
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -0,0 +1,25 @@
+package org.example.lesson2
+
+fun main(){
+
+    val hourOfDeparture = 9
+    val minuteOfDeparture = 39
+    val travelTimeInMinutes = 457
+
+    val hoursOnTheRoad = travelTimeInMinutes / MINUTES_IN_AN_HOUR
+    val minutesOnTheWay = travelTimeInMinutes % MINUTES_IN_AN_HOUR
+
+    var arrivalHour = hourOfDeparture + hoursOnTheRoad
+    var minuteOfArrival = minuteOfDeparture + minutesOnTheWay
+
+    if (minuteOfArrival >= MINUTES_IN_AN_HOUR) {
+        arrivalHour += 1
+        minuteOfArrival %= MINUTES_IN_AN_HOUR
+    }
+
+    val formattedTime = "%02d:%02d".format(arrivalHour, minuteOfArrival)
+
+    println("Час и минута прибытия поезда: $formattedTime")
+}
+
+ val MINUTES_IN_AN_HOUR = 60
