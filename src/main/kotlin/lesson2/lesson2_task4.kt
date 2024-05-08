170e4298fde4baa543b27fbcb184339423f2c456
zaychik2000 <liliyacutie50@gmail.com>
2024-05-08 08:24:36 +0300

    ks-2-4

---
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -0,0 +1,16 @@
+package org.example.lesson2
+
+fun main(){
+
+    val crystalOre = 7
+    val ironОre = 11
+    val buffPercentages = 20
+
+    val crystalOreBonus= crystalOre * (buffPercentages.toFloat() / TOTAL_PERCENT)
+    val ironОreBonus = ironОre * (buffPercentages.toFloat() / TOTAL_PERCENT)
+
+    println("Бонус к кристаллической руде: ${crystalOreBonus.toInt()} \nБонус к железной руде: ${ironОreBonus.toInt()}")
+
+
+}
+val TOTAL_PERCENT = 100
