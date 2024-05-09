c225968eb45bfeff4a20b879b894b2db0c9f6211
zaychik2000 <liliyacutie50@gmail.com>
2024-05-09 13:40:45 +0300

    ks-3-4

---
+++ src/main/kotlin/lesson3/lesson3_task4.kt
@@ -0,0 +1,16 @@
+package org.example.lesson3
+
+fun main (){
+
+    var fromWhere = "E2"
+    var toWhere = "E4"
+    var moveNumber = 1
+
+    println("Откуда - $fromWhere; куда - $toWhere; номер хода - $moveNumber")
+
+    fromWhere = "D2"
+    toWhere = "D3"
+    moveNumber = 2
+
+    println("Откуда - $fromWhere; куда - $toWhere; номер хода - $moveNumber")
+}
