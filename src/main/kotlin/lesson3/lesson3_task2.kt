63f3451897ef776ba5463292e4f5f5430d77cd28
zaychik2000 <liliyacutie50@gmail.com>
2024-05-09 09:07:38 +0300

    ks-3-2

---
+++ src/main/kotlin/lesson3/lesson3_task2.kt
@@ -0,0 +1,19 @@
+package org.example.lesson3
+
+fun main(){
+
+    var surname = "Андреева"
+    val name = "Татьяна"
+    val surname2 = "Сергеевна"
+    var age = 20
+
+    println("ФИО: $surname $name $surname2 \nВозраст: $age")
+
+    surname = "Сидорова"
+    age = 22
+
+    println("\nФИО: $surname $name $surname2 \nВозраст: $age")
+
+
+
+}
