d6aee96a762e68818e52d20e45c86c08075063a0
zaychik2000 <liliyacutie50@gmail.com>
2024-05-09 12:10:29 +0300

    ks-3-3

---
+++ src/main/kotlin/lesson3/lesson3_task3.kt
@@ -0,0 +1,16 @@
+package org.example.lesson3
+
+fun main() {
+
+    val number = 7
+
+    for (number2 in 1..9){
+
+        val multiplicationResult = number * number2
+
+        println("$number x $number = $multiplicationResult")
+    }
+
+
+
+}
