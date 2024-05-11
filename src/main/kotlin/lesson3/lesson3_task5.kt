3f003903b6b7eda1334c55cb2794e591f3409193
zaychik2000 <liliyacutie50@gmail.com>
2024-05-11 10:51:38 +0300

    ks-3-5

--- src/main/kotlin/lesson3/lesson3_task5.kt
+++ src/main/kotlin/lesson3/lesson3_task5.kt
@@ -3,9 +3,7 @@ package org.example.lesson3
 fun main(){
     val inputString = "D2-D4;0"
 
-    val (moves, number) = inputString.split(";")
+    val (fromWhere, toWhere, number) = inputString.split(";", "-")
 
-    val (fromWhere, toWhere) = moves.split("-")
-
-        println("$fromWhere, $toWhere, $number")
+    println("$fromWhere, $toWhere, $number")
 }
